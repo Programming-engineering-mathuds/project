db91b9bff6392d2850ff41e5a27be11cc19de793
PelegBN <pelegbn@gmail.com>
2016-03-26 22:44:40 +0300

    Checklist - add header file

---
+++ CheckList/CheckList/CheckList.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <Windows.h>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+class CheckList
+{
+	string bName;
+	int curY;
+	bool pressed;
+	bool active;
+public:
+	CheckList(string name, int place);
+	void coordinator(COORD& cur, string upDown);
+	void checkSwitch(COORD cur);
+	bool isActive();
+	int getPlace();
+	bool getSwitchState();
+	string getName();
+	void setActive(COORD cur);
+	void setActiveTrue(COORD& cur);
+	void setActiveFalse(COORD cur);
+	~CheckList();
+};
+
