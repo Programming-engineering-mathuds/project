4c9854b7277cc1e4210f1bd077af56ec4a7eca40
adi5785905 <adi5785905@gmail.com>
2016-06-22 21:51:18 +0300

    label refactoring

--- unitedPoject/unitedPoject/Label.cpp
+++ unitedPoject/unitedPoject/Label.cpp
@@ -4,22 +4,6 @@
 #include <stdio.h>
 using namespace std;
 
-//Label::Label(string text, int arrSize, COORD cor, int frameS) :iControl(arrSize, cor), text(text), size(text.length()), frameSize(frameS)
-//{
-//	maxWidth = text.length();
-//	init = { 2, 2 };
-	//print();
-//	putText();
-//	iControl::frame(1, 2);
-
-//}
-//Label::Label() :iControl()
-//{
-	//init = { 2, 2 };
-	//print();
-	//putText();
-//}
-
 Label::Label(int width, string value):iControl(width),text(value),size(value.length())
 {
 	can_Get_Focus = false;
@@ -27,16 +11,20 @@ Label::Label(int width, string value):iControl(width),text(value),size(value.len
 	_layer = 0;
 }
 
+void Label::SetValue(string value)
+{
+	text = value;
+	size = text.length();
+}
+
 void Label::handelInput(INPUT_RECORD input)
 {
 	switch (input.EventType)
 	{
 	case KEY_EVENT: // keyboard input 
-		getInput(input.Event.KeyEvent);
 		break;
 
 	case MOUSE_EVENT: // mouse input 
-		getMouse(input.Event.MouseEvent);
 		break;
 
 	default:
@@ -44,22 +32,9 @@ void Label::handelInput(INPUT_RECORD input)
 	}
 
 }
-void Label::SetValue(string value)
-{
-	text = value;
-	size = text.length();
-	//putText();
-	//print();
-}
 
 void Label::putText()
 {
-	///COORD cur = pos;
-	//COORD cur;
-	//cur.X = pos.X + 1;
-	//cur.Y = pos.Y + 1;
-
-
 	CONSOLE_CURSOR_INFO cci = { 100, FALSE };
 	SetConsoleCursorInfo(hndl, &cci);
 	SetConsoleCursorPosition(hndl, pos);
@@ -69,22 +44,11 @@ void Label::putText()
 	}
 }
 
-void Label::getMouse(MOUSE_EVENT_RECORD mer)
-{
-
-}
-
-void Label::getInput(KEY_EVENT_RECORD key)
-{
-
-}
 void Label::draw(Graphics &g, int left, int top, size_t layer)
 {
 	if (layer > _layer) return;
 	iControl::g = g;
 	COORD cur = { left, top };
-	//pos.X = left;
-	//pos.Y = top;
 	setCoords(cur);
 	putText();
 	frame(1, BorderType::Double);
