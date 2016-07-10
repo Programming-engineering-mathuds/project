5268a3e939baa55d2febd791435ba32eeabd53b6
adi5785905 <adi5785905@gmail.com>
2016-07-10 18:29:19 +0300

    messagebox

--- Final project files/unitedPoject/MessageBox1.h
+++ Final project files/unitedPoject/MessageBox1.h
@@ -10,12 +10,24 @@
 #include "Label.h"
 using namespace std;
 
+struct BoxListener : public MouseListener
+{
+	BoxListener(iControl &c) : _c(c) { }
+	void  MousePressed(Button &b, int x, int y, bool isLeft)
+	{
+		//_c.setForeground(Color::Red);
+		_c.invisible();
+	}
+private:
+	iControl &_c;
+};
 
 class MessageBox1 : public Panel
 {
 	Button button;
 	Label text;
 	Label title;
+	BoxListener listner;
 
 	void setTextLabelSize(int height, int width);
 	void setTitleLabelSize(int height, int width);
@@ -31,7 +43,7 @@ public:
 
 	//void draw(Graphics &g, int left, int top, size_t layer);
 	//void getAllControls(vector < iControl * > *controls){};
-	void mousePressed(int x, int y, bool isLeft){button.mousePressed(x, y, isLeft);	};
+	void mousePressed(int x, int y, bool isLeft){Panel::mousePressed(x, y, isLeft);	};
 	void keyDown(int keyCode, char charater){};
 	void invisible();
 
